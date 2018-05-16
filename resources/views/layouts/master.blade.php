2649aecc71ea108862b9f320ff3f3e52b9f195f1
nomaz_git <m.noman@zameen.com>
2018-05-16 14:27:36 +0500

    first commit to push every thing.

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,121 @@
+<!DOCTYPE html>
+<!--[if lt IE 9 ]><html class="no-js oldie" lang="en"> <![endif]-->
+<!--[if IE 9 ]><html class="no-js oldie ie9" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!-->
+<html class="no-js" lang="en">
+<!--<![endif]-->
+
+<head>
+
+    <!--- basic page needs
+    ================================================== -->
+    <meta charset="utf-8">
+    <title>@yield('title')</title>
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- mobile specific metas
+    ================================================== -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSS
+    ================================================== -->
+    <link rel="stylesheet" href="{{ URL::to('css/base.css') }}">
+    <link rel="stylesheet" href="{{ URL::to('css/vendor.css') }}">
+    <link rel="stylesheet" href="{{ URL::to('css/main.css') }}">
+    <link rel="stylesheet" href="{{ URL::to('css/app.css') }}">
+
+    <!-- script
+    ================================================== -->
+    <script src="js/modernizr.js"></script>
+    <script src="js/pace.min.js"></script>
+
+    <!-- favicons
+    ================================================== -->
+    <link rel="shortcut icon" href="{{ URL::to('favicon.ico') }}" type="image/x-icon">
+    <link rel="icon" href="{{ URL::to('favicon.ico') }}" type="image/x-icon">
+
+</head>
+
+<body id="top">
+    @include('includes.header')
+    
+    @yield('content')
+
+    @include('includes.about')
+
+    @include('includes.services')
+
+    @include('includes.works')
+
+    @include('includes.clients')
+
+    @include('includes.contact')
+
+    @include('includes.footer')
+
+    <!-- photoswipe background
+    ================================================== -->
+    <div aria-hidden="true" class="pswp" role="dialog" tabindex="-1">
+
+        <div class="pswp__bg"></div>
+        <div class="pswp__scroll-wrap">
+
+            <div class="pswp__container">
+                <div class="pswp__item"></div>
+                <div class="pswp__item"></div>
+                <div class="pswp__item"></div>
+            </div>
+
+            <div class="pswp__ui pswp__ui--hidden">
+                <div class="pswp__top-bar">
+                    <div class="pswp__counter"></div><button class="pswp__button pswp__button--close" title="Close (Esc)"></button> <button class="pswp__button pswp__button--share" title=
+                    "Share"></button> <button class="pswp__button pswp__button--fs" title="Toggle fullscreen"></button> <button class="pswp__button pswp__button--zoom" title=
+                    "Zoom in/out"></button>
+                    <div class="pswp__preloader">
+                        <div class="pswp__preloader__icn">
+                            <div class="pswp__preloader__cut">
+                                <div class="pswp__preloader__donut"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="pswp__share-modal pswp__share-modal--hidden pswp__single-tap">
+                    <div class="pswp__share-tooltip"></div>
+                </div><button class="pswp__button pswp__button--arrow--left" title="Previous (arrow left)"></button> <button class="pswp__button pswp__button--arrow--right" title=
+                "Next (arrow right)"></button>
+                <div class="pswp__caption">
+                    <div class="pswp__caption__center"></div>
+                </div>
+            </div>
+
+        </div>
+
+    </div> <!-- end photoSwipe background -->
+
+
+    <!-- preloader
+    ================================================== -->
+    <div id="preloader">
+        <div id="loader">
+            <div class="line-scale-pulse-out">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Java Script
+    ================================================== -->
+    <script src="{{ URL::to('js/jquery-3.2.1.min.js') }}"></script>
+    <script src="{{ URL::to('js/plugins.js') }}"></script>
+    <script src="{{ URL::to('js/main.js') }}"></script>
+    <script src="{{ URL::to('js/app.js') }}"></script>
+
+</body>
+
+</html>
